2f881d513d384df795ada093607f172e8c68edf0
Leonardo Santos <leogilberto2010@hotmail.com>
2024-06-03 14:31:27 -0300

    Adicionado mais configurações

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    MatDialogModule,
+    MatSnackBarModule,
+    provideAnimations(),
+  ],
 };
