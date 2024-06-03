d199e3181dea845977c0ede5f352afd00c3f7f98
Leonardo Santos <leogilberto2010@hotmail.com>
2024-06-03 14:24:13 -0300

    Definido rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { TrashComponent } from './pages/trash/trash.component';
+import { TaskComponent } from './pages/task/task.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+
+  {
+    path: 'trash',
+    component: TrashComponent,
+  },
+
+  {
+    path: 'task/:id',
+    component: TaskComponent,
+  },
+];
